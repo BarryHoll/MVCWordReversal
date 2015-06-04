0e5e36392ca0b1be2bee84ba180eedc39f5fd2e1
BarryHoll <bchollgithub@gmail.com>
2015-06-04 16:03:59 +0100

    Adding model class, WordReversalModel.

---
+++ MVCWordReversal/src/com/bchollywood/mvcwordrev/model/WordReversalModel.java
@@ -0,0 +1,37 @@
+/**
+ * WordReversalModel holds all the data
+ * and performs all the calculations.
+ */
+
+package com.bchollywood.mvcwordrev.model;
+
+public class WordReversalModel {
+	
+	private String reversedWords;
+	private String reversedSentence;
+	
+	public void reverseWords(String sentence) {
+		String[] words = sentence.split(" ");
+		reversedWords = "";
+		for(String word : words){
+			if(reversedWords.equals("")){
+				reversedWords = new StringBuilder(word).reverse().toString();
+			}else{
+				reversedWords = reversedWords + new StringBuilder(word).reverse().toString();
+			}
+		}
+	}
+	
+	public void reverseSentence(String sentence) {
+		reversedSentence = new StringBuilder(sentence).reverse().toString();
+	}
+	
+	public String getReversedWords() {
+		return reversedWords;
+	}
+
+	public String getReversedSentence() {
+		return reversedSentence;
+	}
+
+}
